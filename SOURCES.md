4abb3e1d4718209d90e75a2678fa77f628214ac9
Andrew Lee <acelee500@gmail.com>
2023-02-25 23:44:13 -0500

    Added date selector, priority checker, and buttons

--- SOURCES.md
+++ SOURCES.md
@@ -3,3 +3,6 @@ https://fonts.google.com/specimen/Poppins - for google font
 https://developer.mozilla.org/en-US/docs/Web/API/Element/setAttribute - setting attribute when creating list element
 https://www.w3schools.com/jsref/prop_html_classname.asp - set class name
 https://www.youtube.com/watch?v=MkESyVB4oUw - help remove task from list
+https://www.html-code-generator.com/javascript/drop-down/year-month-day - genreate date selection list
+https://stackoverflow.com/questions/9618504/how-to-get-the-selected-radio-button-s-value - get checked box
+https://stackoverflow.com/questions/8685107/hiding-a-button-in-javascript - button visibility
--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 const tasks = document.getElementById("tasks");
 const history = document.getElementById("history");
 
+let day_selected = (new Date).getDate(); // current day
+let option = '';
+option = '<option>No Due Date</option>'; // first option
+
+for (let i = 1; i < 32; i++) {
+    // value day number with 0. 01 02 03 04..
+    let day = (i <= 9) ? '0' + i : i;
+
+    // or value day number 1 2 3 4..
+    // let day = i;
+
+    let selected = (i === day_selected ? ' selected' : '');
+    option += '<option value="' + day + '"' + selected + '>' + day + '</option>';
+}
+option += '<option value= 31+> 31+ </option>';
+
+
+let due = document.getElementById("day");
+due.innerHTML = option;
+
+
 function addToList() {
     const task = document.getElementById("text").value;
+    const date = document.getElementById("day").value;
 
     if (task === "") {
         alert("Please enter a task");
@@ -12,27 +34,72 @@ function addToList() {
         let input = document.createElement("input");
         input.setAttribute("type", "text");
         input.setAttribute("id", "defaultCheck1");
-        input.setAttribute("value", task);
+        input.setAttribute("value", `${task}`);
         input.setAttribute("readonly", true);
+        input.setAttribute("class", document.querySelector('input[name="types"]:checked').value);
         // console.log(input)
 
-        let btn = document.createElement("button");
-        btn.innerHTML = "complete!";
+        let label = document.createElement("label");
+        label.textContent = `Due Date: ${date} days`;
+
+        let cmp = document.createElement("button");
+        cmp.innerHTML = "complete!";
+        cmp.style.visibility= 'visible';
+
+        let dlt = document.createElement("button");
+        dlt.innerHTML = "delete!";
+        dlt.style.visibility="visible";
+
+        let cnf = document.createElement("button");
+        cnf.innerHTML = "confirm!";
+        cnf.style.visibility= 'hidden';
+
+        let cnl = document.createElement("button");
+        cnl.innerHTML = "cancel!";
+        cnl.style.visibility= 'hidden';
 
         let list = document.createElement("li");
         inputDiv.appendChild(list);
-        inputDiv.appendChild(btn);
+        inputDiv.appendChild(label);
+        inputDiv.appendChild(cmp);
+        inputDiv.appendChild(dlt);
+        inputDiv.appendChild(cnf);
+        inputDiv.appendChild(cnl);
 
         list.appendChild(input);
 
         tasks.appendChild(inputDiv);
 
-        btn.addEventListener("click", () => {
-            tasks.removeChild(inputDiv);
-            history.appendChild(inputDiv);
-            inputDiv.removeChild(btn);
+        cmp.addEventListener("click", () => {
+            cmp.style.visibility= 'hidden';
+            dlt.style.visibility= 'hidden';
+            cnf.style.visibility= 'visible';
+            cnl.style.visibility= 'visible';
 
+            cnf.addEventListener("click", () => {
+                tasks.removeChild(inputDiv);
+                history.appendChild(inputDiv);
+                inputDiv.removeChild(cmp);
+                inputDiv.removeChild(cnl);
+                inputDiv.removeChild(cnf);
+                inputDiv.removeChild(label);
+            });
+
+            cnl.addEventListener("click", () => {
+                cmp.style.visibility = 'visible';
+                cnf.style.visibility= 'hidden';
+                cnl.style.visibility= 'hidden';
+            });
         });
+
+        dlt.addEventListener("click", () => {
+            tasks.removeChild(inputDiv);
+        });
+
+        
+        
+
+
     }
 }
 
